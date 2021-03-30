9d221e05f18b532cd1dc55a38db36a6cfe1c01e9
=최기근 <hap0p@DESKTOP-57EL1RP>
2021-03-30 11:15:18 +0900

    Test 추가

--- src/GitTest.java
+++ src/GitTest.java
@@ -4,6 +4,8 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("Git Test용");
 		System.out.println("수정 push");
+		TestCase test = new TestCase();
+		test.test = "안녕";
 	}
 
 }
